cda216088c295df800f1a1b46d01a6492be60169
emanuelignat <emanuelignat@yahoo.com>
2021-07-06 16:07:58 +0300

    added local settings

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include(todo_urls, namespace="todo")),
     path('api-auth/', include('rest_framework.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
